d9a5e076146aff7f8ae5695fe445eedcae676fb4
joseph okwudi <josephflo55@gmail.com>
2024-02-10 14:16:38 -0700

    create vpc

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0" # Optional but recommended in production
+    }
+  }
+}
+
+
+#provider block
+provider "aws" {
+  region = "us-east-1"
+  # profile = "default"
+}
+
+
 resource "aws_vpc" "test" {
   cidr_block = "10.0.0.0/16"
 
